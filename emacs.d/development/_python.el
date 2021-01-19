dedd7b903f0306512cccdc4a0fa3428cf7c719a6
Charles DeVore <charles.devore@gmail.com>
2021-01-19 00:16:05 -0500

    Add more python settings

--- emacs.d/development/_python.el
+++ emacs.d/development/_python.el
@@ -9,58 +9,44 @@
 ;;; Code:
 
 ;; Define and install python development packages
-(setq python-development-packages
+(defvar python-development-packages
   '(elpy
     py-autopep8
     blacken
     conda
     anaconda-mode
     company-anaconda
+    py-isort
     )
   )
 (mapc 'install-package-if-not-installed python-development-packages)
 
 
-;; (elpy-enable)
-
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
 
-;; Add Company Jedi support
-;; (require 'company-jedi)
-;; (defun my/python-mode-company-jedi-hook ()
-;;   (add-to-list 'company-backends 'company-jedi))
-;; (add-hook 'python-mode-hook 'my/python-mode-company-jedi-hook)
-;; (add-hook 'python-mode-hook 'jedi:setup)
-;; (setq jedi:complete-on-dot t)
+;; Sort the import statements before saving
+(add-hook 'before-save-hook 'py-isort-before-save)
 
-;; (use-package company-jedi
-;;   :ensure t
-;;   :config
-;;   (add-to-list 'company-backends 'company-jedi))
+;; Add autopep8 on save
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
 
 (use-package conda
   :ensure t
   :init
-  (setq conda-anaconda-home "/opt/miniconda3/")
-  (setq conda-env-home-directory "/opt/miniconda3/"))
+  (defvar conda-anaconda-home "/opt/miniconda3/")
+  (defvar conda-env-home-directory "/opt/miniconda3/"))
 
 (eval-after-load "company"
   '(add-to-list 'company-backends '(company-anaconda :with company-capf)))
 
-;; (add-hook 'conda-postactivate-hook 'jedi:stop-server)
-;; (add-hook 'conda-postdeactivate-hook 'jedi:stop-server)
-
-;; (defvar py-shell-name "python3")
-
-;; (defvar python-shell-interpreter "ipython"
-;;       python-shell-interpreter-args "-i --simple-prompt")
 
 ;; (defvar python-shell-completion-native-enable nil)
 
-;; (when (require 'flycheck nil t)
-;;   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-;;   (add-hook 'elpy-mode-hook 'flycheck-mode))
+
+;; Enbable Blacken
+(require 'blacken)
+(add-hook 'python-mode-hook 'blacken-mode)
 
 ;; enable autopep8 formatting on save
 ;; ignoring:
@@ -69,20 +55,31 @@
 ;; - W391 - Remove trailing blank lines.
 ;; - W690 - Fix various deprecated code (via lib2to3).
 
-;; (require 'py-autopep8)
-;; (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
-;; (setq py-autopep8-options '("--ignore=E501,W293,W391,W690"))
-;; (setq py-autopep8-options '("--max-line-length=100"))
-
-;; (define-key global-map (kbd "RET") 'newline-and-indent)
+(require 'py-autopep8)
+(add-hook 'python-mode-hook 'py-autopep8-enable-on-save)
+(setq py-autopep8-options '("--ignore=E501,W293,W391,W690"))
+(setq py-autopep8-options '("--max-line-length=80"))
 
 
 ;; Set tab width to four spaces
 (add-hook 'python-mode-hook
-      (lambda ()
-        (setq indent-tabs-mode t)
-        (setq tab-width 4)
-        (setq python-indent 4)))
+          (lambda ()
+            (setq indent-tabs-mode t)
+            (setq tab-width 4)
+            (setq python-indent 4)))
+
+;; Turn on the fill column indicator for Python
+(add-hook 'python-mode-hook 'fci-mode)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (set-fill-column 80)))
+
+;; Set the python shells
+(add-hook 'python-mode-hook
+          (lambda ()
+            (setq python-shell-interpreter "ipython")
+            (setq python-shell-interpreter-args "-i --simple-prompt")))
+
 
 
 ;; Add support for EIN which provides notebook support
